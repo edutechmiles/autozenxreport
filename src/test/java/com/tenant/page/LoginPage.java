39746a171d3bad10a5b3f336101350afa30ed6a7
edutechmiles <nitinsingh.k2@gmail.com>
2018-10-08 12:09:15 +0530

    Update LoginPage.java

--- src/test/java/com/tenant/page/LoginPage.java
+++ src/test/java/com/tenant/page/LoginPage.java
@@ -8,27 +8,5 @@ import com.report.ZenXReport;
 
 public class LoginPage {
 	
-	public WebDriver driver;
 	
-	
-	
-	
-	public LoginPage(WebDriver driver) {
-		this.driver = driver;
-	}
-	
-	public void login(ZenXReport reportUtil) {
-	
-		//driver.findElement(By.id("login_uname")).sendKeys("rominder@e2e.ch");
-		
-		reportUtil.log("TS-1", "Enter user name", ReportConstants.PASS, "", "");
-		
-		//driver.findElement(By.id("login_password")).sendKeys("Global@123");
-		reportUtil.log("TS-2", "Enter password", ReportConstants.PASS, "", "");
-
-		//driver.findElement(By.id("login_submit")).click();
-		reportUtil.log("TS-3", "Submit form", ReportConstants.PASS, "", "");
-		
-		
-	}
 }
